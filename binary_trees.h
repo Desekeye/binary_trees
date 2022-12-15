b15381c08a010c389609d0ac27cf015e0dfa8a82
Desekeye <triciadede024@gmail.com>
2022-12-15 05:57:38 -0800

    binary

---
+++ binary_trees.h
@@ -0,0 +1,20 @@
+#ifndef _BINARY_TREES_H_
+#define _BINARY_TREES_H_
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+
+/**
+ *
+ * * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+
+struct binary_trees_s
+{}
